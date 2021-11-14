8f336ca0e62153db3e1bc3963451752ac5072e24
Sergio Martin-Albo Calvera <sergiomartinmir@gmail.com>
2021-11-14 22:03:14 +0100

    Ultimos cambios para usar como base

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,14 +1,23 @@
+from typing import Concatenate
 from django.contrib import admin
+from django.forms import forms
 from django.urls import path
 from django.urls.conf import include
 from django.urls.resolvers import URLPattern
 from catalogo.views import todos_libros
 from catalogo.views import LibrosListView
-from catalogo.views import SearchListView
-import debug_toolbar
+from catalogo.views import SearchResultsListView
+from catalogo.views import crear_autor 
+from catalogo.views import CrearAutor, ListaAutores, eliminarAutor, modificarAutor
+
 
 
 urlpatterns = [
     path('libros/', LibrosListView.as_view(), name ='listado_libros'),
-    path('busqueda/', SearchListView.as_view(), name ='search_results'),
+    path('buscarlibros/', SearchResultsListView.as_view(), name ='search_results'),
+    path('autor/crear', crear_autor, name ='search_results'),
+    path('autor/crear2', CrearAutor.as_view(), name='crear_autor2'),
+    path('autores/', ListaAutores.as_view(), name='listado_autor'),
+    path('eliminarAutor/<int:pk>', eliminarAutor.as_view(), name='eliminar_autor'),
+    path('modificarAutor/<int:pk>', modificarAutor.as_view(), name='modificar_autor'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,17 +1,20 @@
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 #from django.http import HttpResponse
 from catalogo.models import Book
 from catalogo.models import Author
 from django.views import generic
-
+from django.views.generic import ListView
+from catalogo.forms import AuthoForm
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
 
 def indice(request):
         '''Pagina Inicial de nuestra Web'''
-        # libros = Book.objects.all()
-        # autores = Author.objects.all()
+        #libros = Book.objects.all()
+        #sautores = Author.objects.all()
         
         datos = {'autores' : autores}
 
@@ -21,10 +24,10 @@ def indice(request):
                 libros = Book.objects.filter(title__icontains = busqueda)
                 datos['noencontrado'] = True
         else:
-                libros = Book.objects.all()
+            libros = Book.objects.all()
 
         datos['libros'] = libros
-       # datos = {'author': 'Alejandro Dumas', 'libros' : libros}
+        datos = {'author': 'Alejandro Dumas', 'libros' : libros}
 
         return render(request, 'index.html', context=datos)
 
@@ -49,19 +52,61 @@ def todos_libros(request):
     return render(request, 'todos_libros.html', context={'libros': libros})
 
 
+#Crear Autor Manual
+def crear_autor(request):
+
+    if request.method == 'POST':
+        form = AuthoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Autor Creado')
+            return redirect('/')
+        else:
+            form = AuthoForm()
+    datos={'form': AuthoForm}
+    return render(request, 'crear_autor.html', context=datos)
+
+#Lista de libros Generica
 class LibrosListView(generic.ListView):
     '''Creacion de Vista Generica'''
     model = Book
     paginate_by = 15
 
-class SearchListView(generic.ListView):
-
+#Buscar libro vista generica
+class SearchResultsListView(ListView):
     model = Book
     context_object_name = 'book_list'
-    template_name = 'libros/search_results.html'
-
-    def get_queryset(self):
+    template_name = 'catalogo/search_results.html'
+    def get_queryset(self): # new
         query = self.request.GET.get('q')
-        return Book.objects.filter(title_icotains = query)
+        return Book.objects.filter(title__icontains=query)
+
+#Crear autor vista generica
+class CrearAutor(SuccessMessageMixin, generic.CreateView):
+    model = Author
+    fields = '__all__'
+    template_name = 'crear_autor.html'
+    success_url = 'catalogo/autores/'
+    success_message = "%(first_name)s %(last_name)s se ha creado correctamente"
+
+#Listar Author vista generica
+class ListaAutores(ListView):
+    '''Vista Generica Autores'''
+    model = Author
+    paginate_by = 15
+
+#Eliminar Autores
+class eliminarAutor(SuccessMessageMixin, generic.DeleteView):
+    model = Author
+    success_url = '/'
+    success_message = "%(first_name)s %(last_name)s se ha borrado correctamente"
+    template_name = 'autor_confirmar_borrado.html'
 
 
+#Modificar Autor de manera Automatica
+class modificarAutor(SuccessMessageMixin, generic.UpdateView):
+    model = Author
+    fields = '__all__'
+    success_url = 'catalogo/autores'
+    template_name = 'modificar_autor.html'
+    success_message = "%(first_name)s %(last_name)s se ha modificado correctamente"
